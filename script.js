92b55dade1e8d0b86101f81a06ea22a76e481144
Grace Gamble <grace.h.gamble@gmail.com>
2018-10-24 16:35:06 -0600

    Creating Objects: Function Constructors

---
+++ script.js
@@ -0,0 +1,40 @@
+// Lecture 2
+// Creating Objects: Function Constructors
+
+// Former way of creating one object
+// var john = {
+//       name: 'John',
+//       yearOfBirth: 1990,
+//       job: 'teacher'
+// };
+
+// Function Constructors, implies that we use a function
+// Capital letter
+var Person = function (name, yearOfBirth, job) {
+      this.name = name;
+      this.yearOfBirth = yearOfBirth;
+      this.job = job;
+      // this.calculateAge = function () {
+      //       console.log(2018 - this.yearOfBirth);
+      // }
+}
+
+// Adds method to prototype
+Person.prototype.calculateAge = function () {
+      console.log(2018 - this.yearOfBirth);
+}
+
+// Adds properties to prototype
+Person.prototype.lastName = 'Smith';
+
+var john = new Person ('John', 1990, 'teacher');
+var jane = new Person ('Jane', 1969, 'designer');
+var mark = new Person ('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
