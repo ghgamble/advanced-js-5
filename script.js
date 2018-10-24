4859ba39975bde968d2115b09053e2bb8ec777b2
Grace Gamble <grace.h.gamble@gmail.com>
2018-10-24 16:45:40 -0600

    The Prototype Chain in the Console

--- script.js
+++ script.js
@@ -10,31 +10,40 @@
 
 // Function Constructors, implies that we use a function
 // Capital letter
-var Person = function (name, yearOfBirth, job) {
-      this.name = name;
-      this.yearOfBirth = yearOfBirth;
-      this.job = job;
+// var Person = function (name, yearOfBirth, job) {
+//       this.name = name;
+//       this.yearOfBirth = yearOfBirth;
+//       this.job = job;
       // this.calculateAge = function () {
       //       console.log(2018 - this.yearOfBirth);
       // }
-}
+// }
 
 // Adds method to prototype
-Person.prototype.calculateAge = function () {
-      console.log(2018 - this.yearOfBirth);
-}
+// Person.prototype.calculateAge = function () {
+//       console.log(2018 - this.yearOfBirth);
+// }
 
 // Adds properties to prototype
-Person.prototype.lastName = 'Smith';
+// Person.prototype.lastName = 'Smith';
+//
+// var john = new Person ('John', 1990, 'teacher');
+// var jane = new Person ('Jane', 1969, 'designer');
+// var mark = new Person ('Mark', 1948, 'retired');
+//
+// john.calculateAge();
+// jane.calculateAge();
+// mark.calculateAge();
+//
+// console.log(john.lastName);
+// console.log(jane.lastName);
+// console.log(mark.lastName);
 
-var john = new Person ('John', 1990, 'teacher');
-var jane = new Person ('Jane', 1969, 'designer');
-var mark = new Person ('Mark', 1948, 'retired');
 
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
 
-console.log(john.lastName);
-console.log(jane.lastName);
-console.log(mark.lastName);
+// Lecture 3
+// The Prototype Chain in the Console
+
+// All in browser console
+// hasOwnProperty
+// instanceof => john instanceof Person === true 
