e420a5ea6a37569220e7a35f91195e63a7134b3a
Grace Gamble <grace.h.gamble@gmail.com>
2018-10-26 16:41:15 -0600

    Closures PART 2

--- script.js
+++ script.js
@@ -226,6 +226,10 @@ interviewQuestion('teacher')('Mark'); */
 
 // Write a function that returns a function that calculates how many years til retirement
 
+// Closures are functions that preserve data
+
+// An inner function always has access to the variables and parameters of its outer function, even after the outer function has returned. Variable object still stays in memory. Scope chain is a pointer to variable objects. Scope chain always stays intact.
+
 function retirement (retirementAge) {
       var a = ' years left until retirement.';
       return function (yearOfBirth) {
@@ -236,5 +240,25 @@ function retirement (retirementAge) {
 
 var retirementUS = retirement(66);
 retirementUS(1990);
-
+// Same but easier to write
 retirement(66)(1990);
+
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementIceland(1990);
+
+function interviewQuestion (job) {
+      return function (name) {
+            if (job === 'designer') {
+                  console.log(name + ', can you please explain what UX design is?');
+            } else if (job === 'teacher') {
+                  console.log('What subject do you teach, ' + name + '?');
+            } else {
+                  console.log('Hello ' + name + ', what do you do?');
+            }
+      }
+}
+
+interviewQuestion('teacher')('John');
