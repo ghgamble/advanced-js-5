f34c390758452123cb3953bd38e47727d6c33151
Grace Gamble <grace.h.gamble@gmail.com>
2018-10-29 16:29:36 -0600

    Coding Challenge PART 1

--- script.js
+++ script.js
@@ -270,7 +270,7 @@ interviewQuestion('teacher')('John'); */
 // Lecture 10
 // Bind, Call, Apply
 
-var john = {
+/* var john = {
       name: 'John',
       age: 26,
       job: 'teacher',
@@ -327,4 +327,42 @@ function isFullAge (limit, el) {
 var ages = arrayCalc(years, calculateAge);
 var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 console.log(ages);
-console.log(fullJapan);
+console.log(fullJapan); */
+
+
+
+// Lecture 11
+// Coding Challenge
+(function() {
+      function Question(question, answers, correct) {
+              this.question = question;
+              this.answers = answers;
+              this.correct = correct;
+      }
+      Question.prototype.displayQuestion = function() {
+            console.log(this.question);
+            for (var i = 0; i < this.answers.length; i++) {
+                  console.log(i + ': ' + this.answers[i]);
+            }
+      }
+      Question.prototype.checkAnswer = function(answer) {
+            if (answer === this.correct) {
+                  console.log('Correct answer!');
+            } else {
+                  console.log('Wrong answer. Try again!');
+            }
+      }
+
+      var q1 = new Question ('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
+      var q2 = new Question ('What is the name of this course\'s teacher?', ['John', 'Micheal', 'Jonas'], 2);
+      var q3 = new Question ('What best describes coding?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+
+      var questions = [q1, q2, q3];
+
+      var n = Math.floor(Math.random() * questions.length);
+
+      questions[n].displayQuestion();
+
+      var answer = parseInt(prompt('Please select the correct answer.'));
+      questions[n].checkAnswer(answer);
+})();
